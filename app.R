ce613ff07ce957dbdf4d7a40edd3bf65091e694e
WHlabmanager <zhangdong_0121@foxmail.com>
2025-07-03 15:06:41 +0800

    Update detail

--- app.R
+++ app.R
@@ -14,14 +14,14 @@ ui <- fluidPage(
     # 确保 Umami 脚本被加载
     tags$script(defer = NA, src = "http://www.bloodecosystem.com:3000/script.js", 
                 `data-website-id` = "a8952081-8633-4fd2-a59f-4b32d27a8227"),
-    # tags$script(HTML("
-    #   // 当 Shiny 完成页面渲染时触发
-    #   $(document).on('shiny:connected', function() {
-    #     if (typeof umami !== 'undefined') {
-    #       umami.trackView(); // 手动记录页面浏览
-    #     }
-    #   });
-    # ")),
+    tags$script(HTML("
+      // 当 Shiny 完成页面渲染时触发
+      $(document).on('shiny:connected', function() {
+        if (typeof umami !== 'undefined') {
+          umami.trackView(); // 手动记录页面浏览
+        }
+      });
+    ")),
   tags$style(HTML("
       /* 全局字体调整 */
       body {
